40fbf484c0d469120a011cfade884d11b6996677
Andreas Rami <andreas.rami@vilango.com>
2026-03-02 14:59:24 +0100

    rfc9457 problem details

---
+++ product/src/main/java/com/vilango/product/ProductExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.vilango.product;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
+
+@RestControllerAdvice
+public class ProductExceptionHandler {
+
+    @ExceptionHandler(ProductApiException.class)
+    ProblemDetail handleProductApiException(ProductApiException ex) {
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(
+                HttpStatus.NOT_FOUND,
+                Objects.requireNonNullElse(ex.getMessage(), "Unknown product error"));
+        problem.setTitle("Product Not Found");
+        return problem;
+    }
+}
