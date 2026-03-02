b5ddab33465a9251c72aa835fd46dab28cd12d4b
Andreas Rami <andreas.rami@vilango.com>
2026-03-02 14:45:26 +0100

    add observability

--- order/src/main/java/com/vilango/order/config/HttpServiceConfig.java
+++ order/src/main/java/com/vilango/order/config/HttpServiceConfig.java
@@ -1,9 +1,7 @@
 package com.vilango.order.config;
 
 import com.vilango.order.product.ProductHttpService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
 import org.springframework.web.service.registry.ImportHttpServices;
 
 @Configuration
---
+++ product/src/main/java/com/vilango/product/otel/InstallOpenTelemetryLogAppender.java
@@ -0,0 +1,22 @@
+package com.vilango.product.otel;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InstallOpenTelemetryLogAppender implements InitializingBean {
+
+	private final OpenTelemetry openTelemetry;
+
+	InstallOpenTelemetryLogAppender(OpenTelemetry openTelemetry) {
+		this.openTelemetry = openTelemetry;
+	}
+
+	@Override
+	public void afterPropertiesSet() {
+		OpenTelemetryAppender.install(this.openTelemetry);
+	}
+
+}
